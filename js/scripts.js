7d94f759bbeae66f13724d421be579901a36269f
twoballs-ai <twoballs20@gmail.com>
2023-07-03 15:58:09 +0300

    add get request  for bubbles data

--- js/scripts.js
+++ js/scripts.js
@@ -6,60 +6,78 @@ let height = canvas.height = innerHeight
 const size = 100
 let current = null
 const NodesID = listNodes()
-const num = NodesID.length
 const elements={}
-// const subelements={}
 const EdgesId = listEdges()
-const subedges = EdgesId.length
-function graphModel() {
 
-  const startElements = {
-    nodes: [
-      { data: { id: 'el0', label: 'Этап 1', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 100, y: 150 } },
-      { data: { id: 'el1', label: 'Этап 2', style:'bubbleWarning', link:'https://www.vk.com' }, position: { x: 250, y: 150 } },
-      { data: { id: 'el2', label: 'Этап 3', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 400, y: 250 } },
-      { data: { id: 'el3', label: 'Этап 4', style:'bubbleSecondary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 100, y: 550 } },
-      { data: { id: 'el4', label: 'Этап 5', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 350, y: 350 } },
-      { data: { id: 'el5', label: 'Этап 6', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 450, y: 450 } },
-      { data: { id: 'el6', label: 'Этап 7', style:'bubbleSecondary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 600, y: 550 } },
-      { data: { id: 'el7', label: 'Этап 8', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 800, y: 650 } },
-      { data: { id: 'el8', label: 'Этап 9', style:'bubbleSecondary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 750, y: 750 } },
-    ],
-    edges: [
-      { data: { id: 'ab', source: 'el0', target: 'el1', label: 'Edge from Node1 to Node2' } },
-      { data: { id: 'ab', source: 'el0', target: 'el3', label: 'Edge from Node1 to Node2' } },
-      { data: { id: 'bc', source: 'el1', target: 'el2', label: 'Edge from Node1 to Node2' } },
-      { data: { id: 'bc', source: 'el2', target: 'el3', label: 'Edge from Node1 to Node2' } },
-      { data: { id: 'ab', source: 'el3', target: 'el4', label: 'Edge from Node1 to Node2' } },
-      { data: { id: 'bc', source: 'el4', target: 'el5', label: 'Edge from Node1 to Node2' } },
-      { data: { id: 'bc', source: 'el5', target: 'el6', label: 'Edge from Node1 to Node2' } },
-      { data: { id: 'ab', source: 'el7', target: 'el8', label: 'Edge from Node1 to Node2' } },
-      { data: { id: 'bc', source: 'el6', target: 'el7', label: 'Edge from Node1 to Node2' } },
-      { data: { id: 'bc', source: 'el6', target: 'el8', label: 'Edge from Node1 to Node2' } },
 
-    ]
+
+async function graphModel() {
+  const url = 'http://127.0.0.1:8000/api/canvas/1'
+
+  
+  try {
+      let res = await fetch(url);
+      return await res.json();
+  } catch (error) {
+      console.log(error);
   }
-  return startElements
+  // const startElements = {
+  //   nodes: [
+  //     { data: { id: 'el0', label: 'Этап 1', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 100, y: 150 } },
+  //     { data: { id: 'el1', label: 'Этап 2', style:'bubbleWarning', link:'https://www.vk.com' }, position: { x: 250, y: 150 } },
+  //     { data: { id: 'el2', label: 'Этап 3', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 400, y: 250 } },
+  //     { data: { id: 'el3', label: 'Этап 4', style:'bubbleSecondary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 100, y: 550 } },
+  //     { data: { id: 'el4', label: 'Этап 5', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 350, y: 350 } },
+  //     { data: { id: 'el5', label: 'Этап 6', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 450, y: 450 } },
+  //     { data: { id: 'el6', label: 'Этап 7', style:'bubbleSecondary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 600, y: 550 } },
+  //     { data: { id: 'el7', label: 'Этап 8', style:'bubblePrimary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 800, y: 650 } },
+  //     { data: { id: 'el8', label: 'Этап 9', style:'bubbleSecondary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 750, y: 750 } },
+  //   ],
+  //   edges: [
+  //     { data: { id: 'ab', source: 'el0', target: 'el1', label: 'Edge from Node1 to Node2' } },
+  //     { data: { id: 'ab', source: 'el0', target: 'el3', label: 'Edge from Node1 to Node2' } },
+  //     { data: { id: 'bc', source: 'el1', target: 'el2', label: 'Edge from Node1 to Node2' } },
+  //     { data: { id: 'bc', source: 'el2', target: 'el3', label: 'Edge from Node1 to Node2' } },
+  //     { data: { id: 'ab', source: 'el3', target: 'el4', label: 'Edge from Node1 to Node2' } },
+  //     { data: { id: 'bc', source: 'el4', target: 'el5', label: 'Edge from Node1 to Node2' } },
+  //     { data: { id: 'bc', source: 'el5', target: 'el6', label: 'Edge from Node1 to Node2' } },
+  //     { data: { id: 'ab', source: 'el7', target: 'el8', label: 'Edge from Node1 to Node2' } },
+  //     { data: { id: 'bc', source: 'el6', target: 'el7', label: 'Edge from Node1 to Node2' } },
+  //     { data: { id: 'bc', source: 'el6', target: 'el8', label: 'Edge from Node1 to Node2' } },
+
+  //   ]
+  // }
+  
 }
+
 
 // функция отрисовки рендеринга страниц, и рендеринга ссылок. 
 function draw() {
-
   const TextBody = document.createElement('div')
   TextBody.id = 'TextBody'
   TextBody.innerHTML = `<h3 class="redactClass">Режим редактора</h3>`;
   document.body.prepend(TextBody)
-  document.addEventListener("contextmenu", onContextMenu,false);
+  canvas.addEventListener("contextmenu", onContextMenu);
+  drawingBubbles()
+  // connectSubAndParent(subelements, elements[parentId])
+  // console.log(elements['el7'])
+}
+
+// функция рисования шаров
+async function drawingBubbles(){
+  const num = (await NodesID).length
+  // console.log(num)
   for (let items = 0; items < num; items++) {
     // subnodes= NodesID[items]['subnodes']
-         // console.log(subnodes)
-    textIntoTheCircle = NodesID[items]['data']['label']
-    link = NodesID[items]['data']['link']
-    positionX = NodesID[items]['position']['x']
-    positionY = NodesID[items]['position']['y']
-    style = NodesID[items]['data']['style']
-    buttonId = NodesID[items]['data']['id']
-      // console.log(buttonId)
+        //  console.log(await NodesID)
+    textIntoTheCircle = (await NodesID)[items]['label']
+    console.log(textIntoTheCircle)
+    link = (await NodesID)[items]['link']
+    positionX = (await NodesID)[items]['posX']
+    positionY = (await NodesID)[items]['posY']
+    style = (await NodesID)[items]['style']
+    buttonId = (await NodesID)[items]['id']
+
     // создание dom-элемента
     const element = document.createElement('div')
     // const textNode = document.createElement('p');
@@ -82,25 +100,30 @@ function draw() {
       startX: 0,
       startY: 0
     }
-  // console.log(textIntoTheCircle)
+  
     // начальное положение
     translate(element, elements[id].x, elements[id].y)
     element.addEventListener("dblclick", {handleEvent: clickBubbles, link: link});
     element.addEventListener("contextmenu",{handleEvent: onContextBubbleMenu, buttonId: buttonId});
   }
-
+  console.log(elements)
   // connect(elements,subelements)
   connect(elements)
-  // connectSubAndParent(subelements, elements[parentId])
-  // console.log(elements['el7'])
 }
+async function listNodes() {
+  // graphModel().then(bubbles => {
+  //   console.log(bubbles) // fetched movies
+  // });
+  let circles = await graphModel()
 
-function listNodes() {
+  // const graph = graphModel()
+  // console.log(circles)
   let itemsNodesID = []
-  const nodesValues = graphModel()['nodes']
+  const nodesValues = circles['nodes']
   const listingNodes = () => {
     nodesValues.forEach(function (item, index, arr) {
       itemsNodesID.push(item)
+      // console.log(item)
     });
   }
   listingNodes()
@@ -108,9 +131,10 @@ function listNodes() {
   return itemsNodesID
 }
 
-function listEdges() {
+async function listEdges() {
+  let circles = await graphModel()
   let itemsEdgesID = []
-  const edgesValues = graphModel()['edges']
+  const edgesValues = circles['edges']
   const listingEdges = () => {
     edgesValues.forEach(function (item, index, arr) {
       itemsEdgesID.push(item)
@@ -222,7 +246,7 @@ window.addEventListener('click', function() {
 
 function translate(el, x, y) {
   el.style.transform = `translate(${x}px, ${y}px)`
-  console.log(x,y)
+  // console.log(x,y)
 }
 
 // function connect(elements) {
@@ -240,12 +264,14 @@ function translate(el, x, y) {
 //       }
 
 // }
-function connect(elements) {
+async function connect(elements) {
   
+  const subedges = (await EdgesId).length
+  console.log(subedges)
   for (let i = 0; i < subedges; i++) {
-    source = EdgesId[i]['data'].source
-    target = EdgesId[i]['data'].target
-  //   console.log(EdgesId[i]['data'])
+    source = (await EdgesId)[i].source
+    target = (await EdgesId)[i].target
+    console.log((await EdgesId)[i].target)
   //  console.log(elements[source])
   //  console.log(elements[target])
   //  console.log(source)
@@ -253,19 +279,14 @@ function connect(elements) {
     // parentidcycle = subelements['el0' + i].parentId
   // console.log(parentidcycle)
   // console.log(elements[parentidcycle].y)
-
       drawLine(
     elements[source].x,
     elements[target].x,
     elements[source].y,
     elements[target].y
   )
-
 }
-
-
 }
-
 
 
 
@@ -279,7 +300,11 @@ function drawLine(x1, x2, y1, y2) {
   context.strokeStyle = "rgba(166, 196, 255, 1)"
   context.stroke()
 }
+function addBubble() {
+  NodesID.push({ data: { id: 'el9', label: 'Этап 10', style:'bubbleSecondary', link:'https://www.youtube.com/watch?v=O1C_fNlZDAU&t=1490s' }, position: { x: 850, y: 850 } })
+  console.log(NodesID)
 
+}
 // function writeMessage(text, posx, posy) {
 //   context.font = '18pt Calibri';
 //   context.fillStyle = 'red';
